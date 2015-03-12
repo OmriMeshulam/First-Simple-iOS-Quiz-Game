6ae769d365e54aa93ff138031605b22736f9413d
Omri Meshulam <omri.meshulam@gmail.com>
2015-03-11 23:34:17 -0400

    Chapter 15 Silver Challenge: Universalize Quiz. Added constraints. Folder refractor.

---
+++ Silver Challenge/Chapter 15/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Omri Meshulam on 2/14/15.
+//  Copyright (c) 2015 OmriMeshulam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
